2da38372399c00f8f0fcc7e40e7d34fa41dbf115
Leonid075 <grebenukleonid075@gmail.com>
2026-06-19 12:17:31 +0300

    fix cat test

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -1,18 +1,23 @@
 from conftest import check
 
 SOURCE = """\
-; cat — посимвольно копирует порт 0 в порт 1, пока есть вход
+; cat — посимвольно копирует порт 0 в порт 1, пока не встретит '\\n'
 .text
 loop:   RD 0
         WR 1
+        CMP (nl)
+        BEZ (done)
         JMP (loop)
+done:   HLT
+.data
+nl:     .word 10
 """
 
 
 def test_cat():
     check(
         SOURCE,
-        inputs={0: "Meow"},
+        inputs={0: "Meow\n"},
         cfg={},
-        expected={1: [77, 101, 111, 119]},
+        expected={1: [77, 101, 111, 119, 10]},
     )
